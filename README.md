8d639ccf7319bb68a100c33a177c161d0b86cd76
imShara <shara@protonmail.com>
2019-11-19 22:53:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# ESP32-CAM XRZ00D1-V240 HTTP Server example with latest ESP-IDF
+HTTP Camera Server example with latest ESP-IDF and chineese (clone?) Ai-Thinker ESP32-CAM module with XRZ00D1-V240 camera module
+
+## What is ESP32-CAM?
+It's module based on ESP32 SOC with camera and SD-card slot
+
+## What is XRZ00D1-V240
+It's OV2640 (or compatible?) camera module which you can get with latest ESP32-CAM modules from Aliexpress, Banggod, etc.
+
+## Is there troubles?
+Yep, it doesn't work out of the box with any example firmware. Module sells without any source codes ¯\_(ツ)_/¯
+
+## How to build
+1. You need latest [ESP-ID SDK](https://github.com/espressif/esp-idf). [How to install it](https://docs.espressif.com/projects/esp-idf/en/latest/get-started/index.html).
+2. Clone repo
+3. Run `idf.py menuconfig` and set WIFI SSID and password (myssid/mypassword is default)
+4. Run `idf.py build`
+
+## How to flash
+1. Short IO0 and GND on ESP-32 module to boot into flash mode
+2. Connect ESP32-CAM to any serial adapter (U0R <— TX, U0T —> RX)
+3. To flash you may run `idf.py -p /dev/ttyUSB0 flash` where `/dev/ttyXXX` is your serial port
+4. After flashing you shoud unconnect IO0 and GND and push RESET button on module to boot new firmware
+
+## How to check
+1. You can use `idf.py monit` to check boot logs
+2. Connect via wifi to it's ssid
+3. Open `http://192.168.4.1/jpg` to make shot 
+
+## Troubleshooting
+Check power source. My module works with 3.3v even 5v, but when powering with 3.3v there is artifacts on image.
+
+If nothing works, try to flash pre-built binary with (esptool)[https://github.com/espressif/esptool]:
+
+```
+esptool.py -p YOUR_SERIAL_PORT_PATH -b 460800 --after hard_reset write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 binary/bootloader.bin 0x8000 binary/partition-table.bin 0x10000 binary/ESP32-CAM_HTTP_AP.bin
+```
+
+## How it made?
+Built with sources of [WiFi softAP example](https://github.com/espressif/esp-idf/tree/master/examples/wifi/getting_started/softAP), [Simple HTTPD Server Example](https://github.com/espressif/esp-idf/tree/master/examples/protocols/http_server/simple), [ESP32 Camera Driver](https://github.com/espressif/esp32-camera) (modified)
+
+ESP32 Camera Driver modified due to this error while starting:
+```
+E (1710) ledc: requested frequency and duty resolution can not be achieved, try reducing freq_hz or duty_resolution. div_param=3
+E (1720) camera_xclk: ledc_timer_config failed, rc=ffffffff
+```
+
+Need to move `periph_module_enable(PERIPH_LEDC_MODULE);` from top to end of function in `/components/esp32-camera/driver/xclk.c` file (thanks @masterd89 for [solution](https://github.com/espressif/esp32-camera/issues/66#issuecomment-526283681))
+
+In menuconfig enabled PSRAM (Serial flasher config ---> Flash size (4 MB))
+
+In menuconfig flash size setted to 4M (Component config ---> ESP32-specific ---> [*] Support for external, SPI-connected RAM)
+
---
+++ main/esp32-cam_http_ap_main.c
@@ -0,0 +1,314 @@
+/* ESP32-CAM XRZ00D1-V240 HTTP Server example with latest ESP-IDF
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include <esp_http_server.h> // HTTP Web Server
+#include "esp_camera.h" // Camera driver (https://github.com/espressif/esp32-camera)
+#include "esp_timer.h" // For shot time measurements
+
+
+/* CONFIG Stuff */
+
+// WIFI config
+#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
+
+static const char *TAG = "ESP32-CAM HTTP AP";
+
+// ESP32-CAM with XRZ00D1-V240 pin map 
+// (https://github.com/Electronlibre2012/ESP-CAM-Ai-Thinker-Remote-Control-Tilt-and-Yaw/issues/2#issuecomment-526942984)
+#define CAM_PIN_PWDN    32
+#define CAM_PIN_RESET   -1
+#define CAM_PIN_XCLK    0
+#define CAM_PIN_SIOD    26
+#define CAM_PIN_SIOC    27
+
+#define CAM_PIN_D7      35
+#define CAM_PIN_D6      34
+#define CAM_PIN_D5      39
+#define CAM_PIN_D4      36
+#define CAM_PIN_D3      21
+#define CAM_PIN_D2      19
+#define CAM_PIN_D1      18
+#define CAM_PIN_D0       5
+#define CAM_PIN_VSYNC   25
+#define CAM_PIN_HREF    23
+#define CAM_PIN_PCLK    22
+
+static camera_config_t camera_config = {
+    .pin_pwdn  = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sscb_sda = CAM_PIN_SIOD,
+    .pin_sscb_scl = CAM_PIN_SIOC,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    // XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG, // YUV422, GRAYSCALE, RGB565, JPEG
+    .frame_size = FRAMESIZE_UXGA, // QQVGA-QXGA Do not use sizes above QVGA when not JPEG
+
+    .jpeg_quality = 15, //0-63 lower number means higher quality
+    .fb_count = 1 //if more than one, i2s runs in continuous mode. Use only with JPEG
+};
+
+
+
+/* WIFI Stuff -------------------------------------------------------------- */
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
+
+void wifi_init_softap(void)
+{
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .max_connection = EXAMPLE_MAX_STA_CONN,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK
+        },
+    };
+    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+}
+
+
+
+/* CAMERA Stuff ------------------------------------------------------------ */
+
+static esp_err_t camera_init(){
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        return err;
+    }
+
+    return ESP_OK;
+}
+
+static esp_err_t camera_capture(){
+    camera_fb_t * fb = esp_camera_fb_get();
+    if (!fb) {
+        ESP_LOGE(TAG, "Camera Capture Failed");
+        return ESP_FAIL;
+    }
+
+    esp_camera_fb_return(fb);
+
+    return ESP_OK;
+}
+
+
+
+/* CAMERA < — > HTTPD Stuff ------------------------------------------------ */
+
+typedef struct {
+        httpd_req_t *req;
+        size_t len;
+} jpg_chunking_t;
+
+static size_t jpg_encode_stream(void * arg, size_t index, const void* data, size_t len){
+    jpg_chunking_t *j = (jpg_chunking_t *)arg;
+    if(!index){
+        j->len = 0;
+    }
+    if(httpd_resp_send_chunk(j->req, (const char *)data, len) != ESP_OK){
+        return 0;
+    }
+    j->len += len;
+    return len;
+}
+
+static esp_err_t jpg_httpd_handler(httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t fb_len = 0;
+    int64_t fr_start = esp_timer_get_time();
+
+    fb = esp_camera_fb_get();
+    if (!fb) {
+        ESP_LOGE(TAG, "Camera capture failed");
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+    res = httpd_resp_set_type(req, "image/jpeg");
+    if(res == ESP_OK){
+        res = httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
+    }
+
+    if(res == ESP_OK){
+        if(fb->format == PIXFORMAT_JPEG){
+            fb_len = fb->len;
+            res = httpd_resp_send(req, (const char *)fb->buf, fb->len);
+        } else {
+            jpg_chunking_t jchunk = {req, 0};
+            res = frame2jpg_cb(fb, 80, jpg_encode_stream, &jchunk)?ESP_OK:ESP_FAIL;
+            httpd_resp_send_chunk(req, NULL, 0);
+            fb_len = jchunk.len;
+        }
+    }
+    esp_camera_fb_return(fb);
+    int64_t fr_end = esp_timer_get_time();
+    ESP_LOGI(TAG, "JPG: %uKB %ums", (uint32_t)(fb_len/1024), (uint32_t)((fr_end - fr_start)/1000));
+    return res;
+}
+
+static const httpd_uri_t jpg = {
+    .uri       = "/jpg",
+    .method    = HTTP_GET,
+    .handler   = jpg_httpd_handler
+};
+
+
+
+/* HTTPD Stuff ------------------------------------------------------------- */
+
+esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
+{
+    httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "Some 404 error message");
+    return ESP_FAIL;
+}
+
+static httpd_handle_t start_webserver(void)
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &jpg);
+        return server;
+    }
+
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
+}
+
+static void stop_webserver(httpd_handle_t server)
+{
+    // Stop the httpd server
+    httpd_stop(server);
+}
+
+static void disconnect_handler(void* arg, esp_event_base_t event_base, 
+                               int32_t event_id, void* event_data)
+{
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server) {
+        ESP_LOGI(TAG, "Stopping webserver");
+        stop_webserver(*server);
+        *server = NULL;
+    }
+}
+
+static void connect_handler(void* arg, esp_event_base_t event_base, 
+                            int32_t event_id, void* event_data)
+{
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server == NULL) {
+        ESP_LOGI(TAG, "Starting webserver");
+        *server = start_webserver();
+    }
+}
+
+
+
+/* MAIN Stuff  ------------------------------------------------------------- */
+
+void app_main(void)
+{
+    static httpd_handle_t server = NULL;
+
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // Initialize wifi
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    wifi_init_softap();
+
+    // Initialize camera
+    ESP_LOGI(TAG, "INIT CAMERA");
+    camera_init();
+
+    // Start HTTP server for the first time
+    ESP_LOGI(TAG, "START HTTP SERVER");
+    server = start_webserver();
+
+    /* Register event handlers to stop the server when Wi-Fi or Ethernet is disconnected,
+     * and re-start it upon connection.
+     */
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+
+    // Try camera capture (just for debug if shit happen)
+    // ESP_LOGI(TAG, "CAMERA CAPTURE");
+    // camera_capture();
+}
